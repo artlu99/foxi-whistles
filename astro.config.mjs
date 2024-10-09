7cd5ac74018d9f01c1490089407e9c96131ea0f6
artlu99 <alu99.sm@gmail.com>
2024-10-09 16:00:46 -0400

    content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://foxi-whistles.vercel.app/",
+  output: "static",
+  adapter: vercelStatic(),
   integrations: [
     tailwind(),
     icon(),

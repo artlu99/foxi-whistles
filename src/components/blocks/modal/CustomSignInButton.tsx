dc523c6d69bc149b5eab5229f1b71cd01713456c
artlu99 <alu99.sm@gmail.com>
2024-10-28 22:30:51 -0400

    siwf button

---
+++ src/components/blocks/modal/CustomSignInButton.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { SignInButton, type StatusAPIResponse } from '@farcaster/auth-kit'
+import { signIn, signOut } from 'auth-astro/client'
+import { useCallback, useState } from 'react'
+import './authKitStyles.css'
+
+function CustomSignInButton() {
+	const [error, setError] = useState(false)
+
+	const getNonce = useCallback(async () => {
+		const nonce = 'abcd1234' // await getCsrfToken();
+		if (!nonce) throw new Error('Unable to generate nonce')
+		return nonce
+	}, [])
+
+	const handleSuccess = useCallback((res: StatusAPIResponse) => {
+		signIn('credentials', {
+			message: res.message,
+			signature: res.signature,
+			name: res.username,
+			pfp: res.pfpUrl,
+			redirect: false
+		})
+	}, [])
+
+	return (
+		<>
+			<SignInButton
+				nonce={getNonce}
+				onSuccess={handleSuccess}
+				onError={() => setError(true)}
+				onSignOut={() => signOut()}
+			/>
+			{error && <div>Unable to sign in at this time.</div>}
+		</>
+	)
+}
+
+export default CustomSignInButton
--- src/components/blocks/modal/Siwf.tsx
+++ src/components/blocks/modal/Siwf.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { AuthKitProvider, SignInButton, type StatusAPIResponse } from '@farcaster/auth-kit'
-import '@farcaster/auth-kit/styles.css'
-import { signIn, signOut } from 'auth-astro/client'
-import { useCallback, useState } from 'react'
+import { AuthKitProvider } from '@farcaster/auth-kit'
+import CustomSignInButton from './CustomSignInButton'
 
 const config = {
 	relay: 'https://relay.farcaster.xyz',
@@ -12,36 +10,12 @@ const config = {
 	domain: 'example.com'
 }
 
-export default function Siwf() {
-	const [error, setError] = useState(false)
-
-	const getNonce = useCallback(async () => {
-		const nonce = 'abcd1234' // await getCsrfToken();
-		if (!nonce) throw new Error('Unable to generate nonce')
-		return nonce
-	}, [])
-
-	const handleSuccess = useCallback((res: StatusAPIResponse) => {
-		signIn('credentials', {
-			message: res.message,
-			signature: res.signature,
-			name: res.username,
-			pfp: res.pfpUrl,
-			redirect: false
-		})
-	}, [])
-
+function Siwf() {
 	return (
 		<AuthKitProvider config={config}>
-			<>
-				<SignInButton
-					nonce={getNonce}
-					onSuccess={handleSuccess}
-					onError={() => setError(true)}
-					onSignOut={() => signOut()}
-				/>
-				{error && <div>Unable to sign in at this time.</div>}
-			</>
+			<CustomSignInButton />
 		</AuthKitProvider>
 	)
 }
+
+export default Siwf

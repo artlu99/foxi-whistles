fea349f9d9a1d4dfb11be7282c7a6fe22198acfc
artlu99 <alu99.sm@gmail.com>
2025-02-03 13:03:05 -0500

    upgrade to Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import auth from "auth-astro";
@@ -9,7 +9,6 @@ import auth from "auth-astro";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sassyhash.artlu.xyz/",
-	output: "hybrid",
 	adapter: vercelServerless(),
 	integrations: [auth(), tailwind(), icon(), sitemap(), react()],
 	security: { checkOrigin: true },

bbf3da4e0d326e53a530ccea16b2919145ea4f21
artlu99 <alu99.sm@gmail.com>
2025-02-23 20:17:07 -0500

    linting

--- src/pages/api/getMyMessages.ts
+++ src/pages/api/getMyMessages.ts
@@ -12,7 +12,7 @@ export async function GET(request: { request: Request }) {
 
 	const url = new URL(request.request.url);
 	const fid = Number.parseInt(url.searchParams.get("fid") || "0");
-	if (isNaN(fid)) {
+	if (Number.isNaN(fid)) {
 		return new Response(JSON.stringify({ message: "Invalid fid parameter" }), {
 			status: 400,
 			headers: { "Content-Type": "application/json" },
@@ -28,7 +28,7 @@ export async function GET(request: { request: Request }) {
 	} catch (error: any) {
 		console.error("Error response:", error.response || error);
 		return new Response(
-			JSON.stringify({ message: "Failed to get casts for fid: " + fid }),
+			JSON.stringify({ message: `Failed to get casts for fid: ${fid}` }),
 			{
 				status: 500,
 				headers: { "Content-Type": "application/json" },
--- src/pages/api/gql.ts
+++ src/pages/api/gql.ts
@@ -1,6 +1,6 @@
-import { GraphQLClient, gql } from "graphql-request";
-import type { ZodObject } from "zod";
-import { GRAPHQL_ENDPOINT, YOGA_READ_TOKEN } from "../../rpc/constants";
+import { GraphQLClient, gql } from 'graphql-request'
+import type { ZodObject, ZodRawShape } from 'zod'
+import { GRAPHQL_ENDPOINT, YOGA_READ_TOKEN } from '../../rpc/constants'
 import {
 	type DatabaseViewResponse,
 	DatabaseViewSchema,
@@ -11,27 +11,27 @@ import {
 	type MyMessagesResponse,
 	MyMessagesSchema,
 	type SharedDatabaseMetricsResponse,
-	SharedDatabaseMetricsSchema,
-} from "../../rpc/types";
+	SharedDatabaseMetricsSchema
+} from '../../rpc/types'
 
 const queries: Record<string, string> = {
-	["getDisabledChannels"]: gql`
+	getDisabledChannels: gql`
 		query getDisabledChannels {
 			getDisabledChannels
 		}
 	`,
-	["getEnabledChannels"]: gql`
+	getEnabledChannels: gql`
 		query getEnabledChannels {
 			getEnabledChannels
 		}
 	`,
-	["sharedDatabaseMetrics"]: gql`
+	sharedDatabaseMetrics: gql`
 		query sharedDatabaseMetrics {
 			numPartitions
 			numSchemas
 		}
 	`,
-	["databaseView"]: gql`
+	databaseView: gql`
 		query dbView {
 			numMessages
 			getTimestampOfEarliestMessage
@@ -39,7 +39,7 @@ const queries: Record<string, string> = {
 			numFids
 		}
 	`,
-	["getDecryptedMessagesByFid"]: gql`
+	getDecryptedMessagesByFid: gql`
 		query getDecryptedMessagesByFid($fid: Int!, $limit: Int, $token: String) {
 			getDecryptedMessagesByFid(
 				limit: $limit
@@ -55,73 +55,68 @@ const queries: Record<string, string> = {
 				}
 			}
 		}
-	`,
-};
+	`
+}
 
 export const getDisabledChannels = async () => {
 	const res = await genericGraphQLQuery<DisabledChannelsResponse>(
-		"getDisabledChannels",
-		DisabledChannelsSchema,
-	);
-	return res;
-};
+		'getDisabledChannels',
+		DisabledChannelsSchema
+	)
+	return res
+}
 
 export const getEnabledChannels = async () => {
 	const res = await genericGraphQLQuery<EnabledChannelsResponse>(
-		"getEnabledChannels",
-		EnabledChannelsSchema,
-	);
-	return res;
-};
+		'getEnabledChannels',
+		EnabledChannelsSchema
+	)
+	return res
+}
 
 export const getSharedDatabaseMetrics = async () => {
 	const res = await genericGraphQLQuery<SharedDatabaseMetricsResponse>(
-		"sharedDatabaseMetrics",
-		SharedDatabaseMetricsSchema,
-	);
-	return res;
-};
+		'sharedDatabaseMetrics',
+		SharedDatabaseMetricsSchema
+	)
+	return res
+}
 
 export const getDatabaseView = async () => {
-	const res = await genericGraphQLQuery<DatabaseViewResponse>(
-		"databaseView",
-		DatabaseViewSchema,
-	);
-	return res;
-};
+	const res = await genericGraphQLQuery<DatabaseViewResponse>('databaseView', DatabaseViewSchema)
+	return res
+}
 
 export const getMyMessages = async (fid: number, limit = 500) => {
 	const res = await genericGraphQLQuery<MyMessagesResponse>(
-		"getDecryptedMessagesByFid",
+		'getDecryptedMessagesByFid',
 		MyMessagesSchema,
-		{ fid, limit, token: YOGA_READ_TOKEN },
-	);
-	return res;
-};
+		{ fid, limit, token: YOGA_READ_TOKEN }
+	)
+	return res
+}
 
 const genericGraphQLQuery = async <T>(
 	queryName: string,
-	schema: ZodObject<any>,
-	variables?: any,
+	schema: ZodObject<ZodRawShape>,
+	variables?: Record<string, unknown>
 ) => {
-	const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT);
+	const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT)
 
-	const query = queries[queryName];
+	const query = queries[queryName]
 	try {
-		const res = await graphQLClient.request<T>(query, variables);
+		const res = await graphQLClient.request<T>(query, variables)
 
-		const validated = schema.safeParse(res);
+		const validated = schema.safeParse(res)
 		if (!validated.success) {
-			throw new Error("Failed to validate response:" + validated.error);
+			throw new Error(`Failed to validate response: ${validated.error}`)
 		}
 
-		return validated.data as T;
+		return validated.data as T
 	} catch (error: any) {
-		console.error("Error response:", error.response || error);
+		console.error('Error response:', error.response || error)
 		throw new Error(
-			"Failed to get " + queryName + variables
-				? ` on: ${JSON.stringify(variables)}`
-				: "",
-		);
+			`Failed to get ${queryName} ${variables ? ` on: ${JSON.stringify(variables)}` : ''}`
+		)
 	}
-};
+}

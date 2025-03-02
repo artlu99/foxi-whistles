dfe3f53eee2ed2bdcf3c0960822146ca83316d00
artlu99 <alu99.sm@gmail.com>
2025-03-01 21:22:07 -0500

    implement rules

--- src/components/blocks/data/MostSeenCastsTable.tsx
+++ src/components/blocks/data/MostSeenCastsTable.tsx
@@ -7,6 +7,21 @@ import { FarcasterEmbed } from 'react-farcaster-embed/dist/client'
 import type { LeaderboardCastInfo } from '../../../rpc/types'
 import { LeaderboardCastInfoResponseSchema } from '../../../rpc/types'
 
+interface SmoothScores {
+	nRaw: number
+	sumRaw: number
+	meanRaw: number
+	stdevRaw: number
+	sumSmooth: number
+	items: {
+		castHash: string
+		raw: number
+		rawZscore: number
+		smooth: number
+		smoothZscore: number
+	}[]
+}
+
 const MAX_PAGE_SIZE = 100
 const MODERATORS: Record<number, string> = {
 	533: 'alexpaden',
@@ -132,7 +147,13 @@ const TableSkeleton = () => (
 						scope="col"
 						className="w-24 px-3 py-2 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
 					>
-						Unique View Attempts
+						Raw Score
+					</th>{' '}
+					<th
+						scope="col"
+						className="w-24 px-3 py-2 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+					>
+						Smoothed Score
 					</th>
 					<th
 						scope="col"
@@ -167,13 +188,54 @@ const TableSkeleton = () => (
 	</div>
 )
 
-interface MostSeenCastsTableProps {
-	fid: number | null
+const calculateSmoothScores = (data: LeaderboardCastInfo[]) => {
+	const smoothData: SmoothScores = {
+		nRaw: 0,
+		sumRaw: 0,
+		sumSmooth: 0,
+		meanRaw: 0,
+		stdevRaw: 0,
+		items: []
+	}
+	for (const cast of data) {
+		smoothData.nRaw++
+		smoothData.sumRaw += cast.count
+		smoothData.items.push({
+			castHash: cast.castHash,
+			raw: cast.count,
+			rawZscore: 0,
+			smooth: 0,
+			smoothZscore: 0
+		})
+	}
+	smoothData.meanRaw = smoothData.sumRaw / smoothData.nRaw
+	smoothData.stdevRaw = Math.sqrt(
+		smoothData.items.reduce((acc, item) => acc + (item.raw - smoothData.meanRaw) ** 2, 0) /
+			smoothData.nRaw
+	)
+	for (const item of smoothData.items) {
+		item.rawZscore = (item.raw - smoothData.meanRaw) / smoothData.stdevRaw
+		item.smoothZscore = Math.atan(item.rawZscore)
+	}
+	for (const item of smoothData.items) {
+		item.smooth = item.smoothZscore * smoothData.stdevRaw + smoothData.meanRaw
+		smoothData.sumSmooth += item.smooth
+	}
+	return smoothData
 }
 
-const MostSeenCastsTable = ({ fid }: MostSeenCastsTableProps) => {
+interface MostSeenCastsTableProps {
+	fid: number | null
+	totalPool: number
+	minPayout: number
+}
+
+const MostSeenCastsTable = ({ fid, totalPool, minPayout }: MostSeenCastsTableProps) => {
 	const [data, setData] = useState<LeaderboardCastInfo[]>([])
 	const [isLoading, setIsLoading] = useState(true)
+
+	const [smoothData, setSmoothData] = useState<SmoothScores>()
+	const [availablePool, setAvailablePool] = useState(totalPool - 10 * minPayout)
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -197,6 +259,17 @@ const MostSeenCastsTable = ({ fid }: MostSeenCastsTableProps) => {
 		}
 		fetchData()
 	}, [fid])
+
+	useEffect(() => {
+		if (data.length) {
+			const smoothData = calculateSmoothScores(data)
+			setSmoothData(smoothData)
+		}
+		// count how many unique FIDs are in the top 10 of the (valid) data
+		const top10Fids = data.slice(0, 10).map((cast) => cast.fid)
+		const uniqueTop10Fids = new Set(top10Fids)
+		setAvailablePool(totalPool - minPayout * uniqueTop10Fids.size)
+	}, [data, totalPool, minPayout])
 
 	if (isLoading) {
 		return (
@@ -231,7 +304,13 @@ const MostSeenCastsTable = ({ fid }: MostSeenCastsTableProps) => {
 								scope="col"
 								className="w-24 px-3 py-2 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
 							>
-								Unique View Attempts
+								Raw Score
+							</th>
+							<th
+								scope="col"
+								className="w-24 px-3 py-2 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+							>
+								Smoothed Score
 							</th>
 							<th
 								scope="col"
@@ -248,17 +327,27 @@ const MostSeenCastsTable = ({ fid }: MostSeenCastsTableProps) => {
 						</tr>
 					</thead>
 					<tbody className="divide-y divide-gray-200 bg-white">
-						{data.map((cast, index) => (
-							<tr key={cast.castHash} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-								<td className="px-3 py-2 text-right text-sm text-gray-500">
-									{cast.count.toLocaleString()}
-								</td>
-								<td className="px-3 py-2">
-									<CastContent cast={cast} />
-								</td>
-								<td className="px-3 py-2 text-center text-sm text-gray-500">{index + 1}</td>
-							</tr>
-						))}
+						{data.map((cast, index) => {
+							const smoothItem = smoothData?.items.find((item) => item.castHash === cast.castHash)
+							const payoutRaw = (availablePool * cast.count) / (smoothData?.sumRaw ?? 1)
+							const payoutSmooth =
+								(availablePool * (smoothItem?.smooth ?? 0)) / (smoothData?.sumSmooth ?? 1)
+
+							return (
+								<tr key={cast.castHash} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+									<td className="px-3 py-2 text-right text-sm text-gray-500">
+										{cast.count.toFixed(2)} (${payoutRaw.toFixed(2)})
+									</td>
+									<td className="px-3 py-2 text-right text-sm text-gray-500">
+										{smoothItem?.smooth.toFixed(2)} (${payoutSmooth.toFixed(2)})
+									</td>
+									<td className="px-3 py-2">
+										<CastContent cast={cast} />
+									</td>
+									<td className="px-3 py-2 text-center text-sm text-gray-500">{index + 1}</td>
+								</tr>
+							)
+						})}
 					</tbody>
 				</table>
 			</div>
